22341a310130a2131eaee690236d9cf697a5e663
St1ch <st1ch@mail.ru>
2018-03-03 17:37:09 +0300

    add coment

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
+const createEnumerableProperty = (propertyName) => { return {propertyName: 'value'}};
+const createNotEnumerableProperty = (propName) => {
+	var notNumber = Symbol(propName);
+	return notNumber;
+};
+const createProtoMagicObject = () => {return Function};
+var count = 0;
+const incrementor = () => {
+	count++;
+	// чтото написал
+ 	return incrementor;
+};
 const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
 const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const createSerializedObject = () => { return null};
 const toBuffer = () => {};
 const sortByProto = () => {};
 
